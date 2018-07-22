1c6c0ffcacbb64c3c776f5be47d1a5d0f8b63cc3
Matt Hill <mlhill1@fullsail.edu>
2018-07-22 12:53:15 -0400

    js validation complete, working on buttons now

--- js/index.js
+++ js/index.js
@@ -5,11 +5,16 @@ const elproductlegend = document.querySelector("#productlegend");
 const elproductimage = document.querySelector("#productimage");
 
 //radio buttons
+const elcolorselect = document.querySelector("#colorselect");
 const elradbtnblack = document.querySelector("#black");
 const elradbtnorange = document.querySelector("#orange");
 const elradbtngrey = document.querySelector("#grey");
 const elradbtncalico = document.querySelector("#calico");
 
+const elsizeselect = document.querySelector("#sizeselect");
+const elradbtnsmall = document.querySelector("#size_small");
+const elradbtnmeduim = document.querySelector("#size_medium");
+const elradbtnlarge = document.querySelector("#size_large");
 //shipping fields
 
 const elname = document.querySelector("#name");
@@ -31,89 +36,262 @@ const eluseraddress2 = document.querySelector("#useraddress2");
 const elcitystatezip = document.querySelector("#citystatezip");
 const elshipcountry = document.querySelector("#shipcountry");
 
+//submit button
+
+const elsubmit = document.querySelector("#submit");
+
+
+
+
+
+/*
+Begin updating Product Choice
+*/
+
+function changeProduct() {
+    let size = document.querySelector('input[name="size"]:checked').value;
+    let color = document.querySelector('input[name="color"]:checked').value;
+    
+    elproductlegend.innerHTML = size +" "+ color + " Cat";
+    elproductimage.src = "images/Cat" + color + ".jpg";
+    elproductimage.alt = "Picture of a " + color + " cat"
+    elsumproductname.innerHTML = size +" "+ color + " Cat";
+
+}
+//cat type event listeners
+elradbtnblack.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtnorange.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtngrey.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtncalico.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtnsmall.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtnmeduim.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+elradbtnlarge.addEventListener('change', function () {
+    changeProduct();
+}, false);
+
+
+
+/*
+End updating Product Choice
+*/
 
 /*
 Begin updating shipping information
 */
 
-elname.addEventListener('keyup', function(){
-    elusername.innerHTML = elname.value;
-},false);
-
-eladdress1.addEventListener('keyup', function(){
-    eluseraddress.innerHTML = eladdress1.value;
-},false);
-
-eladdress2.addEventListener('keyup', function(){
-    eluseraddress2.innerHTML = eladdress2.value;
-},false);
-
-
-function cityStateZip(){
-    elcitystatezip.innerHTML = elcity.value+", "+elstate.value+" "+elzipcode.value;
+function cityStateZip() {
+    elcitystatezip.innerHTML = elcity.value + ", " + elstate.value + " " + elzipcode.value;
 }
 
-elcity.addEventListener('keyup', function(){
-    cityStateZip();
-},false);
-elstate.addEventListener('keyup', function(){
-    cityStateZip();
-},false);
-elzipcode.addEventListener('keyup', function(){
-    cityStateZip();
-},false);
-
-function getCountryName(){
+function getCountryName() {
     let countryvalue = elcountry.value;
-    if(countryvalue === "US"){
+    if (countryvalue === "US") {
         return "";
-    }else{
-        return document.querySelector("option[value='"+countryvalue+"']").innerHTML
+    } else {
+        return document.querySelector("option[value='" + countryvalue + "']").innerHTML
     }
 }
-
-elcountry.addEventListener('change', function(){
-
-    elshipcountry.innerHTML = getCountryName();
-},false);
-
 /*
 End updating shipping information
 */
 
 /*
-Begin updating Product Choice
-
+Begin validating user input
 */
 
-function changeProduct(color){
-    elproductlegend.innerHTML = color+" Cat";
-    elproductimage.src = "images/Cat"+color+".jpg";
-    elproductimage.alt = "Picture of a "+color+" cat"
-    elsumproductname.innerHTML = color+" Cat";
+//validator class
 
+function removeErrors() {
+    let allErrors = document.getElementsByClassName("error");
+    while (allErrors[0]) {
+        console.log("removing " + allErrors[0]);
+        allErrors[0].parentElement.removeChild(allErrors[0]);
+    }
 }
 
-elradbtnblack.addEventListener('change', function(){
-    changeProduct("Black");
-},false);
+class Validator {
+    constructor(input, type) {
+        this.input = input;
+        this.type = type;
+        this.errors = [];
+    }
 
-elradbtnorange.addEventListener('change', function(){
-    changeProduct("Orange");
+    addError(message) {
+        this.errors.push(message);
+    }
 
-},false);
+    getMessages() {
+        const status = this.input.validity;
+        const fieldValue = this.input.value;
+        if (status.typeMismatch && this.type === "email") {
+            this.addError("Please use a valid email address.");
+        }
 
-elradbtngrey.addEventListener('change', function(){
-    changeProduct("Grey");
+        if (status.patternMismatch && this.type === "name") {
+            this.addError("Please use a valid name with only letters and spaces.");
+        }
 
-},false);
+        if (fieldValue.length < 2 && this.type === "name") {
+            this.addError("Please use a valid name with at least two letters.");
+        }
 
-elradbtncalico.addEventListener('change', function(){
-    changeProduct("Calico");
-},false);
+        if (fieldValue.length === 0) {
+            this.addError("This is a required field.");
+        }
+
+        if (status.patternMismatch && this.type === "state") {
+            this.addError("Please use your 2 or 3 letter state or province abbreviation.");
+        }
+
+        if (status.patternMismatch && this.type === "zip") {
+            this.addError("Please use your 5 or 9 digit zip code");
+        }
+
+        return this.errors;
+    }
+}
+
+
+
+elsubmit.addEventListener("click", (event) => {
+
+    //reset error count and displays
+    let errorcount = 0;
+    event.preventDefault();
+    removeErrors();
+
+    //check email for errors
+    let validateEmail = new Validator(elemail, "email");
+    let emailErrors = validateEmail.getMessages();
+
+    if (emailErrors.length > 0) {
+        errorcount++;
+        emailErrors.forEach((err) => {
+            elemail.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+
+    //check name for errors
+    let validateName = new Validator(elname, "name");
+    let nameErrors = validateName.getMessages();
+
+    if (nameErrors.length > 0) {
+        errorcount++;
+        nameErrors.forEach((err) => {
+            console.log(err);
+            elname.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+
+    //checking address line 1
+    let validateAddress = new Validator(eladdress1, "address");
+    let addressErrors = validateAddress.getMessages();
+
+    if (addressErrors.length > 0) {
+        errorcount++;
+        addressErrors.forEach((err) => {
+            console.log(err);
+            eladdress1.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+
+    //checking city 
+    let validateCity = new Validator(elcity, "city");
+    let cityErrors = validateCity.getMessages();
+
+    if (cityErrors.length > 0) {
+        errorcount++;
+        cityErrors.forEach((err) => {
+            console.log(err);
+            elcity.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+
+    //checking state 
+    let validateState = new Validator(elstate, "state");
+    let stateErrors = validateState.getMessages();
+
+    if (stateErrors.length > 0) {
+        errorcount++;
+        stateErrors.forEach((err) => {
+            console.log(err);
+            elstate.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+
+    //checking state 
+    let validateZip = new Validator(elzipcode, "zip");
+    let zipErrors = validateZip.getMessages();
+
+    if (zipErrors.length > 0) {
+        errorcount++;
+        zipErrors.forEach((err) => {
+            console.log(err);
+            elzipcode.insertAdjacentHTML('afterend', "<p class='error'>" + err + "</p>")
+        })
+    }
+    if(errorcount === 0){
+        alert("form submitted")
+    }
+});
+
+
+
 /*
-End updating Product Choice
+End validating user input
 */
 
+
+/*
+Begin event listeners for Shipping Details
+*/
+elname.addEventListener('keyup', function () {
+    elusername.innerHTML = elname.value;
+}, false);
+
+eladdress1.addEventListener('keyup', function () {
+    eluseraddress.innerHTML = eladdress1.value;
+}, false);
+
+eladdress2.addEventListener('keyup', function () {
+    eluseraddress2.innerHTML = eladdress2.value;
+}, false);
+
+elcity.addEventListener('keyup', function () {
+    cityStateZip();
+}, false);
+
+elstate.addEventListener('keyup', function () {
+    cityStateZip();
+}, false);
+
+elzipcode.addEventListener('keyup', function () {
+    cityStateZip();
+}, false);
+
+elcountry.addEventListener('change', function () {
+    elshipcountry.innerHTML = getCountryName();
+}, false);
+/*
+End event listeners for Shipping Details
+*/
 
 console.log("JS Loaded")
